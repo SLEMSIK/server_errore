b134653031a23be367edac12a3b457d127dda6a5
SLEMSIK <84916321+SLEMSIK@users.noreply.github.com>
2022-06-25 20:25:36 +0300

    Update data.py

--- data.py
+++ data.py
@@ -2,4 +2,29 @@ import pyfiglet
 import time
 print(pyfiglet.figlet_format("VYACHESLAV system")) 
 time.sleep(3)
-print("К сожалению произошла ошибка доступа к зашифрованному каналу связи VYACHESLAV SYSTEM!") 
+info = input("К сожалению произошла ошибка доступа к зашифрованному каналу связи VYACHESLAV SYSTEM!\n") 
+if info == "server.get_data":
+  print(pyfiglet.figlet_format("SERVER DATA") 
+  print("Статус сервера - 200") 
+  time.sleep(3)
+  print("Зашифрованный канал - OK")
+  time.sleep(2)
+  print("Заблокированых каналов - 1")
+  time.sleep(5)
+  print("Не определённых каналов - 649")
+  time.sleep(3)
+  print("Помех для каналов - 17")
+if info == "server.channel_restart":
+  time.sleep(10)
+  print("Каналы успешно обновлены")
+  data1 = input("restart system\n") 
+  if data1 == "yes":
+        print(pyfiglet.figlet_format("VYACHESLAV system") 
+        time.sleep(2)
+        print("Доступ к системе успешно установлен!") 
+              
+             
+              
+              
+              
+            
